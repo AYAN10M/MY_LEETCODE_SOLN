118ef9dfbbaf7dcc72f79e83e81a240e34c6f2d4
AYAN10M <gopalchandrahaldar1997@gmail.com>
2024-09-20 19:55:33 +0530

    two_sum problem started

---
+++ two_sum.c
@@ -0,0 +1,17 @@
+/*
+    problem link: https://leetcode.com/problems/two-sum/description/?envType=problem-list-v2&envId=array
+
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    
+}
